73e146d5f1727d2c49b213c672db7a2444330b16
Steve Kinney <hello@stevekinney.net>
2018-08-14 13:53:29 -0500

    Very important branding update

--- src/components/Application.js
+++ src/components/Application.js
@@ -11,7 +11,7 @@ import Styles from '../styles.css';
 const Application = () => (
   <div className={Styles.container}>
     <header className={Styles.header}>
-      <h1 className={Styles.header__title}>Noted</h1>
+      <h1 className={Styles.header__title}>Super Important Website of Notes</h1>
     </header>
     <NoteListContainer />
     <Switch>
